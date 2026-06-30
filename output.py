06d5fe1eeb523be62113cb94d08106076a970ab4
lourdes-privitera <lulitaprivi@gmail.com>
2026-06-30 18:42:37 -0300

    Creo el menu de opciones

---
+++ output.py
@@ -0,0 +1,11 @@
+def mostrar_menu() -> None:
+    print("=========== GESTIÓN DE SALUMNOS ===========")  
+    print("")    
+    print("1.   Registrar alumno")
+    print("2.   Listado alumnos")
+    print("3.   Buscar alumno")
+    print("4.   Modificar alumno")
+    print("5.   Eliminar alumno")
+    print("6.   Ver estadísticas")
+    print("7.   Salir")
+
