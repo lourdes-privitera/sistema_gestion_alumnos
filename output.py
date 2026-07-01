16cdd3355f56caca88ec41f59e06cbc7561db662
lourdes-privitera <lulitaprivi@gmail.com>
2026-06-30 23:12:24 -0300

    Guardo la funcion para mostrar estadisticas

--- output.py
+++ output.py
@@ -45,3 +45,57 @@ def mostrar_busqueda_alumno(lista_alumnos:list) -> None:
     else:
         print("Alumno inexistente.")
         
+def listar_alumnos(lista_alumnos:list) -> None:
+    """Muestra todos los alumnos registrados.
+
+    Args:
+        lista_alumnos (list): Lista de alumnos.
+    """
+
+    print("\n======= LISTADO DE ALUMNOS =======\n")
+
+    for alumno in lista_alumnos:
+
+        mostrar_alumno(alumno)
+
+        print("-----------------------------")
+
+def mostrar_estadisticas(lista_alumnos:list)->None:
+    """Muestra las estadísticas generales de los alumnos.
+
+    Informa:
+        - Cantidad total de alumnos.
+        - Promedio de notas.
+        - Alumno con mayor nota.
+        - Cantidad de aprobados.
+        - Cantidad de desaprobados.
+
+    Args:
+        lista_alumnos (list): Lista de alumnos registrados.
+    """    
+
+    from estadisticas import (
+        cantidad_alumnos,
+        promedio_notas,
+        mejor_alumno,
+        cantidad_aprobados,
+        cantidad_desaprobados
+    )
+
+    print("\n===== ESTADÍSTICAS =====")
+
+    print(f"Cantidad de alumnos: {cantidad_alumnos(lista_alumnos)}")
+
+    print(f"Promedio: {promedio_notas(lista_alumnos):.2f}")
+
+    mejor = mejor_alumno(lista_alumnos)
+
+    if len(lista_alumnos) > 0:
+
+        mejor = mejor_alumno(lista_alumnos)
+
+        print(f"Mayor nota: {mejor['nombre']} {mejor['apellido']} ({mejor['nota']})")
+
+    print(f"Aprobados: {cantidad_aprobados(lista_alumnos)}")
+
+    print(f"Desaprobados: {cantidad_desaprobados(lista_alumnos)}")
