343ad86c61ef454344af133334f328a4985aa42f
lourdes-privitera <lulitaprivi@gmail.com>
2026-06-30 21:57:49 -0300

    Creo las funcione para mostrar las opciones 2 y 3

--- output.py
+++ output.py
@@ -1,3 +1,9 @@
+from inputs import(
+    ingresar_dni
+)
+from alumnos import(
+    buscar_alumno
+)
 def mostrar_menu() -> None:
     print("=========== GESTIÓN DE SALUMNOS ===========")  
     print("")    
@@ -9,3 +15,33 @@ def mostrar_menu() -> None:
     print("6.   Ver estadísticas")
     print("7.   Salir")
 
+#La función recibe un diccionario y accede a cada dato utilizando la clave correspondiente.
+def mostrar_alumno(alumno:dict) -> None:
+    """Muestra los datos de un alumno.
+
+    Args:
+        alumno (dict): Diccionario con los datos del alumno.
+    """
+
+    print(f"DNI: {alumno['dni']}")
+    print(f"Nombre: {alumno['nombre']}")
+    print(f"Apellido: {alumno['apellido']}")
+    print(f"Edad: {alumno['edad']}")
+    print(f"Nota: {alumno['nota']}")
+
+def mostrar_busqueda_alumno(lista_alumnos:list) -> None:
+    """Solicita un DNI, busca el alumno y muestra sus datos.
+
+    Args:
+        lista_alumnos (list): Lista de alumnos registrados.
+    """
+
+    dni = ingresar_dni()
+
+    alumno = buscar_alumno(lista_alumnos, dni)
+
+    if alumno:
+        mostrar_alumno(alumno)
+    else:
+        print("Alumno inexistente.")
+        
